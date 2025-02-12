320755eb148f1b8cf08c240304ad0c9d84ff1ab9
Nathaniel Laursen <nathaniel.m.laursen@gmail.com>
2025-02-12 13:13:06 -0700

    Tried to figure out collisisons

--- vehicle_body_3d.gd
+++ vehicle_body_3d.gd
@@ -1,23 +1,4 @@
-#extends CharacterBody3D
-#@export var speed = 100
-#@export var fallAcceleration = 150
-#var zeroVector = Vector3.ZERO
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#print("ready")
-	#pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass 
-	#
-#func _physics_process(delta: float) -> void:
-	#var velocityVector = Vector3.ZERO
-	#velocityVector.z = 1 * speed
-	#velocity = velocityVector
-	#move_and_slide()
-extends CharacterBody3D
+extends VehicleBody3D
 
 # How fast the player moves in meters per second.
 @export var speed = 140
@@ -46,17 +27,21 @@ func _physics_process(delta):
 		direction.z += 10
 #
 	#if direction != Vector3.ZERO:
-		direction = direction.rotated(normalAxis, angle)
+		#direction = Vector3(0, 0, 1).rotated(normalAxis, angle)
 		#direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction)
+		#$Pivot.basis = Basis.looking_at(direction)
 
 	# Ground Velocity
 	#target_velocity.x = direction.x * speed
 	#target_velocity.z = direction.z * speed
+	steering = angle
+	#print(steering)
+	#print(is_on_floor())
+
 	# Vertical Velocity
 	#if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		#target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
+	#velocity = target_velocity
+	#move_and_slide()
