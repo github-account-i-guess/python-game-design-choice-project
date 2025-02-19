5cd6a3eb1c77940fa22a9a99ab779288a41af5bc
Nathaniel Laursen <nathaniel.m.laursen@gmail.com>
2025-02-19 13:13:52 -0700

    Tried to improve drifting (it is slightly better)

--- vehicle_body_3d.gd
+++ vehicle_body_3d.gd
@@ -1,5 +1,5 @@
 extends VehicleBody3D
-@export var added_engine_force = 30
+@export var added_engine_force = 3000
 
 @export var angle = 0
 
@@ -15,21 +15,18 @@ func _physics_process(delta):
 	var steeringCap = PI/3
 
 	var drifting = Input.is_action_pressed("drift")
-	$BackLeftWheel.use_as_traction = not drifting
-	$BackRightWheel.use_as_traction = not drifting
-	#$FrontLeftWheel.use_as_traction = not drifting
-	#$FrontRightWheel.use_as_traction = not drifting
-	if Input.is_action_pressed("move_right") and angle > -steeringCap:
+	var right = Input.is_action_pressed("move_right")
+	var left = Input.is_action_pressed("move_left")
+	if right and angle > -steeringCap:
 		#direction.x -= 10
 		angle -= 0.05
-	elif  Input.is_action_pressed("move_left") and angle < steeringCap:
+	elif left and angle < steeringCap:
 		#direction.x += 10	
 		angle += 0.05
 	else:
-		angle -= 0.01 * sign(angle)
+		angle -= 0.01 * sign(angle) * (1 + speed/30)
 	if Input.is_action_pressed("move_forward"):
-		if engine_force < 10000:
-			engine_force += added_engine_force
+		engine_force = added_engine_force
 	else:
 		##brake = 0.2
 		engine_force -= 100
@@ -37,14 +34,20 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_back"):
 		if speed > 10:
 			brake = 2.0
-		engine_force -= 200
+		engine_force = -added_engine_force/2
 	if drifting:
-		steeringCap = PI/4
-		boost += 5
+		#steeringCap = PI/4
+		if (left or right) and boost < 100:
+			boost += 1
+		engine_force = 0
+		$BackLeftWheel.wheel_friction_slip = 8
+		$BackRightWheel.wheel_friction_slip = 8
 	else:
 		if boost > 0:
-			engine_force += boost * 5
-			boost -= 1
+			engine_force += boost * 5000
+			boost -= 10
+		$BackLeftWheel.wheel_friction_slip = 12.5
+		$BackRightWheel.wheel_friction_slip = 12.5
 	#engine_force = speedMap(accelTime)
 	$Camera3D.setFov(sqrt(speed) + 90)
 	print(speed)
