46a4af2803486d11551e86e74d20f0e8409544f7
github-account-i-guess <nathaniel.m.laursen@gmail.com>
2025-02-21 16:57:22 -0700

    blessed drifting

--- vehicle_body_3d.gd
+++ vehicle_body_3d.gd
@@ -18,6 +18,7 @@ func _ready():
 	#print(wheels)
 	for wheel in wheels:
 		wheel.wheel_roll_influence = 0
+		wheel.wheel_friction_slip = 8
 
 func _physics_process(delta):
 	#print(delta)
@@ -31,7 +32,7 @@ func _physics_process(delta):
 	var drifting = Input.is_action_pressed("drift")
 	var right = Input.is_action_pressed("move_right")
 	var left = Input.is_action_pressed("move_left")
-	if Input.is_action_pressed("move_forward") and not drifting:
+	if Input.is_action_pressed("move_forward"):
 		engine_force = added_engine_force
 	else:
 		##brake = 0.2
@@ -50,6 +51,8 @@ func _physics_process(delta):
 		if speed > 10:
 			brake = 2.0
 		engine_force = -added_engine_force/2
+	#if Input.is_action_just_pressed("drift"):
+			#linear_velocity += Vector3(0, 30, 0)
 	if drifting:
 		#steeringCap = PI/4
 		if (left or right) and boost < 48:
@@ -62,9 +65,9 @@ func _physics_process(delta):
 		#for wheel in frontWheels:
 			#wheel.wheel_friction_slip = 10
 		if left:
-			$BackLeftWheel.wheel_roll_influence = 20
+			$BackLeftWheel.wheel_roll_influence = 10
 		if right:
-			$BackRightWheel.wheel_roll_influence = 20
+			$BackRightWheel.wheel_roll_influence = 10
 					
 		#$FrontLeftWheel.wheel_friction_slip = 10
 		#$FrontRightWheel.wheel_friction_slip = 10
@@ -81,14 +84,19 @@ func _physics_process(delta):
 			#if boost < 2 or abs(steering) < 1:
 				#
 		#else:
-		$BodyMesh.mesh.material.albedo_color = Color(1, 0, 0, 0.5)		
+		$BodyMesh.mesh.material.albedo_color = Color(0, 1, 0, 0.5)		
 		for wheel in backWheels:
 			wheel.wheel_roll_influence = 0
 	if Input.is_action_just_released('drift') and not Input.is_action_pressed("move_back"):
-		var direction = Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), global_rotation.y)
+		var direction = Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), steering + global_rotation.y)
+		var increasedDirection = direction.normalized() * 10 * (0.3 + boost/4) 
+		#increasedDirection.y += 5
 		print(direction)
-		linear_velocity += direction.normalized() * 10 * (0.3 + boost/4) 
-		boost = 0	
+		#linear_velocity *= 99/100
+		linear_velocity += increasedDirection
+		boost = 0
+	#if Input.is_action_just_pressed("jump"):
+		#linear_velocity += Vector3(0, 30, 0)
 	#engine_force = speedMap(accelTime)
 	#for wheel in wheels:
 		#print(wheel.wheel_roll_influence)
@@ -98,10 +106,15 @@ func _physics_process(delta):
 	steering = angle
 	#if linear_velocity.y >= 0:
 		#print(speed)
-	#print(speed - prevSpeed)
+	print(speed - prevSpeed)
 	#prevSpeed = speed
 	#rotation.x = 0
 	#rotation.z = 0
 	#if (left != right):
 		#linear_velocity.z += 1
+	#if int(linear_velocity.length()) % 100 == 50:
+		##linear_velocity *- 0
+		#$BodyMesh.mesh.material.albedo_color = Color(1, 0, 0, 0.5)		
+		#linear_velocity += Vector3(0, 5, 0)
+		
 	
