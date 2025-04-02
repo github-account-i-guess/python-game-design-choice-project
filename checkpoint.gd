6c87907963bcace156aa329f344cefdeb64bfdb9
Thumb-1 <audrey@unusualia.com>
2025-04-02 12:48:05 -0600

    map

--- checkpoint.gd
+++ checkpoint.gd
@@ -1,6 +1,6 @@
 extends Area3D
 
-@export var checkpointNum = 0
+@export var checkpointNum = 4
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
